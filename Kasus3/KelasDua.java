18483c993369675ec64c8995c332f193e5f8ad13
Ariana Rahmawati <98734080+arianarmw@users.noreply.github.com>
2022-09-17 22:53:14 +0700

    mengerjakan kasus pembeda objek dan class

---
+++ Kasus3/KelasDua.java
@@ -0,0 +1,20 @@
+/* Author : Ariana Rahmawati
+ * Date   : September 15th 2022
+ * Lesson : Object, Class, Encapsulation
+ * Course : Object Oriented Programming (OOP)
+ */
+
+package Kasus3;
+
+public class KelasDua {
+	{
+		System.out.println(5);
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(6);
+		KelasSatu satu = new KelasSatu();
+		KelasSatu dua = new KelasSatu(10);
+	}
+}
---
+++ Kasus3/KelasSatu.java
@@ -0,0 +1,25 @@
+/* Author : Ariana Rahmawati
+ * Date   : September 15th 2022
+ * Lesson : Object, Class, Encapsulation
+ * Course : Object Oriented Programming (OOP)
+ */
+
+package Kasus3;
+
+class KelasSatu {
+	{
+		System.out.println(11);
+	}
+	
+	static {
+		System.out.println(2);
+	}
+	
+	public KelasSatu(int i) {
+		System.out.println(3);
+	}
+	
+	public KelasSatu() {
+		System.out.println(4);
+	}
+}
